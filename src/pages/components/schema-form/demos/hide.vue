416e32e1bba4b89334195f34c96d71946d2b81b0
aibayanyu20 <aibayanyu@qq.com>
2024-04-29 20:04:10 +0800

    feat: add table schame

--- src/pages/components/schema-form/demos/hide.vue
+++ src/pages/components/schema-form/demos/hide.vue
@@ -14,22 +14,39 @@ const schemas = shallowRef<SchemaItem[]>([
   {
     dataIndex: 'test',
     title: '测试1',
-    depend: ['test1', 'test2']
+    depend: ['test1', 'test2'],
+    valueEnum: {
+      A: {
+        text: 'A',
+      },
+      B: {
+        text: 'B'
+      },
+      C: {
+        text: 'C'
+      }
+    }
   },
   {
     dataIndex: 'test1',
     title: '测试2',
     hideInForm: true,
     onDepend(params, action) {
-      console.log('asd')
+      if (params.value === 'A')
+        action.setHideInForm('test1', false)
+      else
+        action.setHideInForm('test1', true)
     },
   },
   {
     dataIndex: 'test2',
-    title: '测试2',
+    title: '测试3',
     hideInForm: true,
     onDepend(params, action) {
-      console.log('Sd')
+      if (params.value === 'B')
+        action.setHideInForm('test2', false)
+      else
+        action.setHideInForm('test2', true)
     },
   }
 ])
